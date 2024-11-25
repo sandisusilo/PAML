85b45f96cc085a6f6fa72e507603b9172e970e9d
sandi susilo <santuy132109@gmail.com>
2024-11-26 01:46:04 +0700

    warna

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#00FFFF',
+    backgroundColor: '#FF0000',
   },
   text: {
     fontSize: 30,
